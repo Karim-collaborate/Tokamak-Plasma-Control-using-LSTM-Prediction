778adc757790c70f1b3d9c4f1a4f9421a4311aca
Karim <karimoolahiane9@gmail.com>
2026-04-21 13:56:46 -0400

    Change: name of model function from diruption_model to disruption_detection model

--- src/models/LSTM_model.py
+++ src/models/LSTM_model.py
@@ -2,7 +2,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM
 
 # AI model to predict plasma disruptions
-def disruption_model(input_shape):
+def disruption_detection_model(input_shape):
     model = Sequential([
         LSTM(64, return_sequences=True, input_shape=input_shape),
         LSTM(32),
