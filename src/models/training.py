704e7ab181e720888bc141b0a98109f2220d8d7d
Karim <karimoolahiane9@gmail.com>
2026-04-22 02:51:08 -0400

    Fix: compiling before fitting and visualizualization problems

--- src/models/training.py
+++ src/models/training.py
@@ -5,9 +5,15 @@ import matplotlib.pyplot as plt
 
 # Model training 
 X_train, y_train = generate_synthetic_data()
-model = disruption_detection_model(input_shape=(X_train.shape[1], X_train.shape[2]))
-model.fit(X_train, y_train, epochs=10, batch_size=32)
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-model.save("models/lstm_model.keras")
-plt.plot(history.history['loss'])
 
+model = disruption_detection_model(input_shape=(X_train.shape[1], X_train.shape[2]))
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+history = model.fit(X_train, y_train, epochs=10, batch_size=32)
+
+model.save("lstm_model.keras")
+
+plt.plot(history.history['accuracy'])
+plt.title("Model accuracy")
+plt.show()
