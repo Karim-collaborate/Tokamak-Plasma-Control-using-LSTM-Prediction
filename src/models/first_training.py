ad321a0728b3ef0676e8cc60185d35a2bbccd864
Karim <karimoolahiane9@gmail.com>
2026-04-22 03:14:09 -0400

    Change: training.py to first_training.py

---
+++ src/models/first_training.py
@@ -0,0 +1,19 @@
+from src.data import generate_synthetic_data 
+from src.models.LSTM_model import disruption_detection_model 
+import matplotlib.pyplot as plt 
+
+
+# Model training 
+X_train, y_train = generate_synthetic_data()
+
+model = disruption_detection_model(input_shape=(X_train.shape[1], X_train.shape[2]))
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+history = model.fit(X_train, y_train, epochs=10, batch_size=32)
+
+model.save("src/models/lstm_model.keras")
+
+plt.plot(history.history['accuracy'])
+plt.title("Model accuracy")
+plt.show()
